ff2be2faa19ca80effd20a3a4cc1feff7d986bbf
User <User@DESKTOP-QFL41E2>
2024-10-13 10:04:15 +0200

    DbContextChange

---
+++ FIndMeARoommate/RoommateDbContext.cs
@@ -0,0 +1,29 @@
+﻿using FIndMeARoommate.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace FIndMeARoommate
+{
+    public class RoommateDbContext : DbContext
+    {
+        public RoommateDbContext()
+        {
+        }
+        public RoommateDbContext(DbContextOptions<RoommateDbContext> options) :
+        base(options)
+        { }
+        public DbSet<Students> Students { get; set; }
+        public DbSet<Dormitories> Dormitories { get; set; }
+        public DbSet<Applications> Applications { get; set; }
+        public DbSet<Announcements> Announcements { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
